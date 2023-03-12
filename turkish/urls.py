692a34c04e6c4d9ffe20a85b883d508dcb6fe044
Yerassyliitu <34080@iitu.edu.kz>
2023-03-12 23:08:20 +0600

    heroku deploy7

--- turkish/urls.py
+++ turkish/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from turkish import settings
-from django.views.static import serve
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 
