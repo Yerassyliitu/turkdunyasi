b00ccf0fe087f3e87b4c5fdc2dc22e5b9cefc36f
Yerassyliitu <34080@iitu.edu.kz>
2023-03-12 23:17:01 +0600

    heroku deploy8

--- turkish/settings.py
+++ turkish/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'g8$6*83j(88052%8iq3#t#3ug4jci*4r3x64tstonzd^9aw(-a'
 
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
