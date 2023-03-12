c4f0ce87015a31cf36bb82e4d65065e0f3514543
Yerassyliitu <34080@iitu.edu.kz>
2023-03-12 23:22:50 +0600

    add sqlite3

--- turkish/settings.py
+++ turkish/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'g8$6*83j(88052%8iq3#t#3ug4jci*4r3x64tstonzd^9aw(-a'
 
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -84,14 +83,10 @@ WSGI_APPLICATION = 'turkish.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'postgres',
-       'USER': 'postgres',
-       'PASSWORD': '1004',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Database engine to use
+        'NAME': BASE_DIR / 'db.sqlite3',         # Database file path
+    }
 }
 
 
