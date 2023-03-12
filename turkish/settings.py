ad2fcac431fdf336d8e848a12716b2e88c68ee02
Yerassyliitu <34080@iitu.edu.kz>
2023-03-12 23:52:12 +0600

    work with database start

--- turkish/settings.py
+++ turkish/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'turkish.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Database engine to use
-        'NAME': BASE_DIR / 'db.sqlite3',         # Database file path
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
