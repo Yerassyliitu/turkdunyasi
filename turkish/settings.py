d4a8d830b0702848088c9a6750b40201f5b73aa0
Yerassyliitu <34080@iitu.edu.kz>
2023-03-13 11:47:08 +0600

    replace static to staticfiles2

--- turkish/settings.py
+++ turkish/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+    BASE_DIR / 'static'
 ]
 
 
